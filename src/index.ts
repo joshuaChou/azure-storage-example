f6530b3ee8010ac2d31a91ddc3ce6013d064b1f4
joshua.chou <joeyuehong@gmail.com>
2017-07-07 15:26:21 +0800

    add new container method

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,10 @@ const commandDown = process.env.npm_config_down
 
 const commandUpl = process.env.npm_config_upload
 
+const commandCreate = process.env.npm_config_create
+
+const commandList = process.env.npm_config_list
+
 const help = process.argv.slice(2)[0];
 
 const uploadFolder = 'upload'
@@ -24,7 +28,42 @@ const uploadFolder = 'upload'
 const downloadFolder = 'out'
 
 export default class CarSharing {
-    constructor() { }
+    constructor() {
+        this.checkDirectorySync('out')
+        this.checkDirectorySync('upload')
+    }
+
+    /**
+     * 建立container
+     * @param containername 
+     */
+    createContainer(containername: string) {
+        blobSvc.createContainerIfNotExists(containername, (error, result, response) => {
+            if (!error) {
+                console.log('create ok'.green)
+            } else {
+                console.log(error)
+            }
+        })
+    }
+
+    listAllContainers() {
+        blobSvc.listContainersSegmented(null, (error, result, response) => {
+            if (!error) {
+                if (result.entries.length > 1) {
+                    console.log('===========List All Containers=============')
+                    result.entries.forEach((element: any) => {
+                        console.log(color.green(element.name))
+                    });
+                    console.log('=====================================')
+                } else {
+                    console.log('Container No files'.cyan);
+                }
+            } else {
+                console.log(error)
+            }
+        })
+    }
 
     /**
      * 上傳檔案。
@@ -50,16 +89,16 @@ export default class CarSharing {
         return new Promise((resolve, reject) => {
             blobSvc.listBlobsSegmented(containerName, null, (error, result, response) => {
                 if (!error) {
-                    if(result.entries.length>1){
-                        console.log('===========List All file=============')
+                    if (result.entries.length > 1) {
+                        console.log('===========List ' + defaultContainerName + ' containter All file=============')
                         result.entries.forEach((element: any) => {
                             console.log(color.green(element.name))
                         });
                         console.log('=====================================')
-                    }else{
+                    } else {
                         console.log('Container No files'.cyan);
                     }
-                   
+
                 }
 
                 return resolve(result.entries)
@@ -76,6 +115,8 @@ export default class CarSharing {
      * @param filePath 
      */
     downloadFile(containerName: string, filename: string, filePath: string) {
+        this.checkDirectorySync(path.parse(filePath).dir)
+
         blobSvc.getBlobToStream(containerName, filename, fs.createWriteStream(filePath), (error, result, response) => {
             if (!error) {
                 console.log('download OK'.green)
@@ -99,38 +140,69 @@ export default class CarSharing {
             }
         })
     }
+
+    /**
+     * 檢查與建立目錄
+     * @param directory 
+     */
+    private checkDirectorySync(directory: string) {
+        try {
+            fs.statSync(directory);
+        } catch (e) {
+            fs.mkdirSync(directory);
+        }
+    }
+
+
 }
 
 const carshare = new CarSharing()
 
-let returnEntities = null
 
-if (help) {
-  console.log(`
+if (commandCreate) {
+    let containerName = process.argv.slice(2)[0];
+    if (containerName) {
+        carshare.createContainer(containerName);
+    } else {
+        console.error("please enter container name".red)
+    }
+}
+
+if (help === 'help') {
+    console.log(`
 ============================================================
         azure storage command list
 ============================================================
 
-example:npm start --del
+example:npm start --list
 
+list  list all container and car's blob
 del   del all car share folder files
 upload upload all file in upload folder
 down  download all files in car share folder
+create [container name] create new container
 
 `)
-}else{
-    returnEntities = carshare.listFolder(defaultContainerName)
+
 }
 
+function getEntities() {
+    carshare.listAllContainers();
+    return carshare.listFolder(defaultContainerName)
+
+}
 
 if (commandUpl) {
+    let folder = uuid.v4();
     fs.readdirSync(uploadFolder).forEach(file => {
-     carshare.upLoadImage(defaultContainerName, uuid.v4() + '.jpg', path.join(uploadFolder,file))
+        carshare.upLoadImage(defaultContainerName, folder + '/' + uuid.v4() + '.jpg', path.join(uploadFolder, file))
     })
-   
+
 }
 
 if (commandDown) {
+    let returnEntities = getEntities()
+
     returnEntities.then((result) => {
         result.forEach((item: any) => {
             carshare.downloadFile(defaultContainerName, item.name, path.resolve('out', item.name))
@@ -139,6 +211,7 @@ if (commandDown) {
 }
 
 if (commandDel) {
+    let returnEntities = getEntities();
     returnEntities.then((result) => {
         result.forEach((item: any) => {
             carshare.deleteFile(defaultContainerName, item.name)
@@ -146,6 +219,11 @@ if (commandDel) {
     })
 }
 
+
+
+if (commandList) {
+    getEntities()
+}
 
 
 
